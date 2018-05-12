aa2271e8a31ad23571182b2e42f7b9a51d562608
Cody Ley-Han <codyleyhan@gmail.com>
2018-05-12 07:52:05 -0700

    update mocks for new interfaces

--- mocks/CarStore.go
+++ mocks/CarStore.go
@@ -3,7 +3,6 @@ package mocks
 
 import mock "github.com/stretchr/testify/mock"
 import models "github.com/ucladevx/BPool/models"
-import postgres "github.com/ucladevx/BPool/stores/postgres"
 
 import stores "github.com/ucladevx/BPool/stores"
 
@@ -58,29 +57,6 @@ func (_m *CarStore) GetByID(id string) (*models.Car, error) {
 	return r0, r1
 }
 
-// GetByWhere provides a mock function with given fields: fields, queryModifiers
-func (_m *CarStore) GetByWhere(fields []string, queryModifiers []stores.QueryModifier) ([]postgres.CarRow, error) {
-	ret := _m.Called(fields, queryModifiers)
-
-	var r0 []postgres.CarRow
-	if rf, ok := ret.Get(0).(func([]string, []stores.QueryModifier) []postgres.CarRow); ok {
-		r0 = rf(fields, queryModifiers)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).([]postgres.CarRow)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func([]string, []stores.QueryModifier) error); ok {
-		r1 = rf(fields, queryModifiers)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // GetCount provides a mock function with given fields: queryModifiers
 func (_m *CarStore) GetCount(queryModifiers []stores.QueryModifier) (int, error) {
 	ret := _m.Called(queryModifiers)
@@ -102,13 +78,13 @@ func (_m *CarStore) GetCount(queryModifiers []stores.QueryModifier) (int, error)
 	return r0, r1
 }
 
-// Insert provides a mock function with given fields: user
-func (_m *CarStore) Insert(user *models.Car) error {
-	ret := _m.Called(user)
+// Insert provides a mock function with given fields: car
+func (_m *CarStore) Insert(car *models.Car) error {
+	ret := _m.Called(car)
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(*models.Car) error); ok {
-		r0 = rf(user)
+		r0 = rf(car)
 	} else {
 		r0 = ret.Error(0)
 	}
